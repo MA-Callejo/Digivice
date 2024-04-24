5847a8a5aebdc0884cfeffff941edaf016681449
Miguel Ángel <miguelangel.callejo@blautic.com>
2024-04-24 19:18:23 +0200

    Nuevo sistema de menus

--- Menu.py
+++ Menu.py
@@ -2,28 +2,53 @@ from math import ceil
 
 import pygame
 
-
 class Menu:
     fila = 0
     columna = 0
     columnas = 3
     iconos = []
+    submenus = []
     size = 40
     offset = 2
     colorFondo = (244, 204, 204)
     adapter = pygame.image.load("src/img/iconos/menuAdapter.png")  # 40x 40
     selectedAdapter = pygame.image.load("src/img/iconos/menuAdapterSelected.png")  # 40x 40
+
+    def getSubmenu(self):
+        return self.submenus[(self.fila * self.columnas) + self.columna]
+
     def dibujar(self, screen):
         screen.fill(self.colorFondo)
         for i in range(ceil(len(self.iconos)/self.columnas)):
             for j in range(self.columnas):
-                if (self.fila == i) and (self.columna == j):
-                    marco = self.adapter
-                else:
+                if (self.columna == i) and (self.fila == j):
                     marco = self.selectedAdapter
+                else:
+                    marco = self.adapter
                 screen.blit(marco, (2 + ((self.size + self.offset)*i), 2 + ((self.size + self.offset)*j)))
+                screen.blit(self.iconos[(i*self.columnas)+j], (7 + (42 * i), 7 + (42 * j)))
 
-
+    def setDireccion(self, direccion):  # Right: 0, Up: 1, Left: 2, Down: 3
+        if direccion == 0:
+            if self.columna < self.columnas - 1:
+                self.columna = self.columna + 1
+            else:
+                self.columna = 0
+        if direccion == 1:
+            if self.fila == 0:
+                self.fila = ceil(len(self.iconos)/self.columnas) - 1
+            else:
+                self.fila = self.fila - 1
+        if direccion == 2:
+            if self.columna == 0:
+                self.columna = self.columnas - 1
+            else:
+                self.columna = self.columna - 1
+        if direccion == 3:
+            if self.fila < ceil(len(self.iconos)/self.columnas) - 1:
+                self.fila = self.fila + 1
+            else:
+                self.fila = 0
 
 
 class subMenu(Menu):
@@ -35,4 +60,34 @@ class subMenu(Menu):
 
 
 class MainMenu(Menu):
-    iconos = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    iconos = [
+        pygame.image.load("src/img/iconos/grafico-de-torta.png"),
+        pygame.image.load("src/img/iconos/restaurante.png"),
+        pygame.image.load("src/img/iconos/banera.png"),
+        pygame.image.load("src/img/iconos/cama.png"),
+        pygame.image.load("src/img/iconos/control-de-juego.png"),
+        pygame.image.load("src/img/iconos/drogas.png"),
+        pygame.image.load("src/img/iconos/carrito-de-compras.png"),
+        pygame.image.load("src/img/iconos/limpieza-de-datos.png"),
+        # pygame.image.load("src/img/iconos/mapa.png"),
+        pygame.image.load("src/img/iconos/ajustes.png"),
+    ]
+    submenus = []
+
+class MenuController():
+    currentMenu = None
+    menus = []
+    def isActive(self):
+        return len(self.menus) > 0
+    def enter(self):
+        if len(self.menus) == 0:
+            self.menus.append(MainMenu())
+        else:
+            self.menus.append(self.menus[-1].getSubmenu())
+    def esc(self):
+        if len(self.menus) > 0:
+            self.menus.pop()
+    def dibujar(self, screen):
+        self.menus[-1].dibujar(screen)
+    def setDireccion(self, direccion):
+        self.menus[-1].setDireccion(direccion)
--- main.py
+++ main.py
@@ -2,147 +2,57 @@ import pygame as pygame, sys
 from pygame.locals import *
 
 from Fondo import Fondo
+from Menu import MainMenu, MenuController
 
 pygame.init()
 size = (128, 128)
 screen = pygame.display.set_mode(size)
 
 fondo = Fondo()
+menu = MenuController()
+isMenu = False
 colorFondo = (244, 204, 204)
-MENU_ITEMS = 8
 clock = pygame.time.Clock()
-menuAdapter = pygame.image.load("src/img/iconos/menuAdapter.png")  # 43x43
-menuAdapterSelected = pygame.image.load("src/img/iconos/menuAdapterSelected.png")  # 43x43
-menu = False
-menu_selected = 0
-submenu = False
-submenuIndex = 0
-submenu_selected = 0
-MAX_PISO = 1
-MAX_HABITACIONES = [2, 1]
-habitaciones = [[pygame.image.load("src/img/fondos/jardin.png"), pygame.image.load("src/img/fondos/salon.png"), pygame.image.load("src/img/fondos/cocina.png")],
-                [pygame.image.load("src/img/fondos/dormitorio.png"), pygame.image.load("src/img/fondos/banno.png")]]
 
 
 def leerEventos():
-    global menu, menu_selected, submenu_selected, submenu, fondo
+    global fondo, isMenu, menu
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                menu = not menu
+                isMenu = not isMenu
             if event.key == pygame.K_UP:
-                if menu:
-                    if menu_selected - 3 >= 0:
-                        menu_selected = menu_selected - 3
+                if menu.isActive():
+                    menu.setDireccion(1)
                 else:
                     fondo.setDireccion(1)  # Right: 0, Up: 1, Left: 2, Down: 3
             if event.key == pygame.K_DOWN:
-                if menu:
-                    if menu_selected + 3 <= MENU_ITEMS:
-                        menu_selected = menu_selected + 3
+                if menu.isActive():
+                    menu.setDireccion(3)
                 else:
                     fondo.setDireccion(3)  # Right: 0, Up: 1, Left: 2, Down: 3
             if event.key == pygame.K_LEFT:
-                if menu:
-                    if menu_selected - 1 >= 0:
-                        menu_selected = menu_selected - 1
+                if menu.isActive():
+                    menu.setDireccion(2)
                 else:
                     fondo.setDireccion(2)  # Right: 0, Up: 1, Left: 2, Down: 3
             if event.key == pygame.K_RIGHT:
-                if menu:
-                    if menu_selected + 1 <= MENU_ITEMS:
-                        menu_selected = menu_selected + 1
+                if menu.isActive():
+                    menu.setDireccion(0)
                 else:
                     fondo.setDireccion(0)  # Right: 0, Up: 1, Left: 2, Down: 3
             if event.key == pygame.K_a:
-                if menu:
-                    if menu_selected == 0:
-                        mostrarEstats()
-                    if menu_selected == 1:
-                        menuComida()
-                    if menu_selected == 2:
-                        ducha()
-                    if menu_selected == 3:
-                        menuLimpieza()
-                    if menu_selected == 4:
-                        menuCompra()
-                    if menu_selected == 5:
-                        menuAjustes()
-                    if menu_selected == 6:
-                        menuJuegos()
-                    if menu_selected == 7:
-                        dormir()
-                    if menu_selected == 8:
-                        mapa()
-                    if menu_selected == 9:
-                        menuMedicina()
-                else:
-                    if submenu:
-                        pass
-                    else:
-                        menu = True
+                menu.enter()
             if event.key == pygame.K_b:
-                if menu:
-                    menu = False
-                    menu_selected = 0
-                if submenu:
-                    submenu_selected = 0
-                    submenu = False
-                    menu = True
-
-submenu_items = []
-def mostrarEstats():
-    pass
-
-def menuComida():
-    submenuIndex = 1
-    submenu_items = ["Arroz"]
-    submenu = True
-
-def ducha():
-    pass
-
-def menuLimpieza():
-    submenuIndex = 2
-    submenu_items = ["Jardin", "Salon", "Cocina", "Cuarto", "Baño"]
-
-def menuCompra():
-    pass
-
-def menuAjustes():
-    pass
-
-def menuJuegos():
-    pass
-
-def dormir():
-    pass
-
-def mapa():
-    pass
-
-def menuMedicina():
-    pass
+                menu.esc()
 
 
 def dibujarPantalla():
     screen.fill(colorFondo)
-    if menu:
-        for i in range(MENU_ITEMS + 1):
-            if i == menu_selected:
-                marco = menuAdapterSelected
-            else:
-                marco = menuAdapter
-            screen.blit(marco, (2 + (i % 3) * 42, 2 + (i // 3) * 42))
-    elif submenu:
-        for i in range(len(submenu_items) + 1):
-            if i == submenu_selected:
-                marco = menuAdapterSelected
-            else:
-                marco = menuAdapter
-            screen.blit(marco, (2 + (i % 3) * 42, 2 + (i // 3) * 42))
+    if menu.isActive():
+        menu.dibujar(screen)
     else:
         fondo.dibujar(screen)
     pygame.display.flip()
